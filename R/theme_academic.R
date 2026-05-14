f580489650def574de6dbbcd9259826531b7df9a
Nick Katz <nickkatz575@gmail.com>
2026-05-14 17:10:52 +0000

    added theme and min_max help page

--- R/theme_academic.R
+++ R/theme_academic.R
@@ -1,3 +1,12 @@
+#' Academic Theme in addition to ggplot2
+#'
+#' @param base_size A positive integer value
+#'
+#' @returns plot with theme added when passed with ggplot2 (centralized plot title, grid lines removed)
+#' @importFrom ggplot theme_classic theme element_blank  element_text
+#' @export
+#'
+#' @examples
 theme_academic <- function(base_size = 12)
 {
   ggplot2::theme_classic(base_size = base_size) +
